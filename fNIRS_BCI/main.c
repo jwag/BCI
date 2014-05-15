96d002760f3347c76477f0d8b5b5f95c00542dfd
Jacob Wagner <w.jacobwagner@gmail.com>
2014-05-15 18:56:26 -0500

    We now have 6 PWM's working.  I will write function that can change the pwm of each channel to a desired duty cycle.

--- fNIRS_BCI/main.c
+++ fNIRS_BCI/main.c
@@ -252,7 +252,7 @@ void ADC3_CH12_DMA_Config(void)
 }
 //***************************************************************************************
 /**
-  * @brief  Configure the TIM3 and TIM2 Ouput Channels.
+  * @brief  Configure the TIM3 and TIM4 Ouput Channels.
   * @param  None
   * @retval None
   */
@@ -260,8 +260,8 @@ void TIM_Config(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    /* TIM3 and TIM2 clock enable */
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM2, ENABLE);
+    /* TIM3 and TIM4 clock enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4, ENABLE);
 
     /* GPIOC and GPIOB clock enable */
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOB, ENABLE);
@@ -274,8 +274,8 @@ void TIM_Config(void)
     GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-    /* GPIOB Configuration:  TIM3 CH3 (PB0) and TIM3 CH4 (PB1) and TIM2 CH3 (PB10) and TIM2 CH4 (PB11)*/
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_10 | GPIO_Pin_11;
+    /* GPIOB Configuration:  TIM3 CH3 (PB0) and TIM3 CH4 (PB1) and TIM4 CH3 (PB8) and TIM4 CH4 (PB9) */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_8 | GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -287,8 +287,8 @@ void TIM_Config(void)
     GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_TIM3);
     GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM3);
     GPIO_PinAFConfig(GPIOB, GPIO_PinSource1, GPIO_AF_TIM3);
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_TIM2);
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_TIM2);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_TIM4);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_TIM4);
 
 }
 //***************************************************************************************
@@ -327,22 +327,20 @@ void PWM_Config()
     PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 28000000) - 1;
 
     /* Time base configuration */
-    TIM_TimeBaseStructure.TIM_Period = 665;
+    TIM_TimeBaseStructure.TIM_Period = 8900;
     TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 
     TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
-    /* Compute the prescaler value */
-    //PrescalerValue = (uint16_t) ((SystemCoreClock ) / 28000000) - 1;
-    //TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+    // To set Duty cycle just set this and if 50% duty cycle is desired
+    // Then take 8900*(decimal duty Cycle to get TIM_Pulse
     /* PWM1 Mode configuration: Channel1 */
     TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+    TIM_OCInitStructure.TIM_Pulse = 4450;
     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 
     TIM_OC1Init(TIM3, &TIM_OCInitStructure);
@@ -379,25 +377,25 @@ void PWM_Config()
     /* PWM1 Mode configuration: Channel3 */
     TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = 50;
+    TIM_OCInitStructure.TIM_Pulse = 2000;
     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 
-    TIM_OC1Init(TIM2, &TIM_OCInitStructure);
+    TIM_OC3Init(TIM4, &TIM_OCInitStructure);
 
-    TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
 
     /* PWM1 Mode configuration: Channel4 */
     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = 50;
+    TIM_OCInitStructure.TIM_Pulse = 4450;
 
-    TIM_OC2Init(TIM2, &TIM_OCInitStructure);
+    TIM_OC4Init(TIM4, &TIM_OCInitStructure);
 
-    TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
 
     /* TIM3 enable counter */
     TIM_Cmd(TIM3, ENABLE);
-    /* TIM2 enable counter */
-    TIM_Cmd(TIM2, ENABLE);
+    /* TIM enable counter */
+    TIM_Cmd(TIM4, ENABLE);
 }
 
 /* This funcion initializes the USART1 peripheral
